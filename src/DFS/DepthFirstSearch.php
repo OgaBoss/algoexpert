6f50e91a5dcecbd6e3608f194658940f9f26aed2
oluwadamilolaadebayo <goshensoftinc@gmail.com>
2020-01-06 13:08:16 +0100

    - WIP

--- src/DFS/DepthFirstSearch.php
+++ src/DFS/DepthFirstSearch.php
@@ -9,8 +9,12 @@ class DepthFirstSearch
     public function depthFirstSearch(array $array, $child): array  {
         $array[] = $child->name;
         foreach ($child->children as $node) {
+            if (count($node->children) === 0) {
+                continue;
+            }
             $this->depthFirstSearch($array, $node);
         }
+
 
         $this->array = [...$this->array, ...$array];
         return array_unique($this->array);
