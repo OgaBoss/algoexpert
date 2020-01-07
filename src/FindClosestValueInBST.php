0ed69bf501f3121d599bb8e0fea105493b2fe73e
oluwadamilolaadebayo <goshensoftinc@gmail.com>
2020-01-07 10:25:36 +0100

    set up class for finding closest value in BST

---
+++ src/FindClosestValueInBST.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Src\Algorithm;
+
+
+class FindClosestValueInBST
+{
+    public function findValueWithRecursion($value, $tree): int  {
+
+    }
+
+    public function findValueWithIteration($value, $tree): int {
+
+    }
+
+    private function recursiveHelper($value, $tree, $closest) {
+        if (isNull($tree)) {
+            return $closest;
+        }
+
+
+    }
+}
