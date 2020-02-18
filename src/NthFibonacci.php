a44fa00a1bf3e785e229b239a0f28f38e3be9fa3
oluwadamilolaadebayo <goshensoftinc@gmail.com>
2020-02-18 12:37:35 +0100

    implemented 3 methods fopr nth fibonacci

---
+++ src/NthFibonacci.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace Src\Algorithm;
+
+
+class NthFibonacci
+{
+    public array $fibonacci = [];
+
+    /**
+     * Time: O(2^n) | Space: O(n)
+     *
+     * @param $number
+     * @return int
+     */
+    public function recursive($number)
+    {
+        if ($number === 1) {
+            return 1;
+        } elseif ($number === 2) {
+            return 2;
+        } else {
+            return $this->recursive($number - 1) + $this->recursive($number - 2);
+        }
+    }
+
+    /**
+     * Time: O(n) | Space O(n)
+     *
+     * @param $number
+     * @param $cache
+     * @return mixed
+     */
+    public function memoize($number, $cache)
+    {
+        if (array_key_exists($number, $cache)) {
+            return $cache[$number];
+        } else {
+            $cache[$number] = $this->memoize($number - 1, $cache) + $this->memoize($number - 2, $cache);
+            return $cache[$number];
+        }
+    }
+
+    /**
+     * Time: O(n) | Space O(1)
+     *
+     * @param $number
+     * @return mixed
+     */
+    public function optimized($number) {
+        $lastTwo = [0, 1];
+        $counter = 3;
+         while ($counter <= $number) {
+             $nextFib = $lastTwo[0] + $lastTwo[1];
+             $lastTwo[0] = $lastTwo[1];
+             $lastTwo[1] = $nextFib;
+             $counter++;
+         }
+
+         if ($number > 1) {
+             return $lastTwo[1];
+         }
+
+         return $lastTwo[0];
+    }
+}
